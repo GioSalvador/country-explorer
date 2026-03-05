d4dd2875d19aef46ee359414f4e67e51d7397273
GioSalvador <strokemusic07@gmail.com>
2026-03-05 18:16:47 -0300

    feat: implement lazy loading for country routes to improve initial load performance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
-import { CountryList } from './features/countries/pages/country-list/country-list';
-import { CountryDetail } from './features/countries/pages/country-detail/country-detail';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'countries', pathMatch: 'full' },
-  { path: 'countries', component: CountryList },
-  { path: 'countries/:code', component: CountryDetail },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/countries/pages/country-list/country-list').then((m) => m.CountryList),
+  },
+  {
+    path: 'countries/:code',
+    loadComponent: () =>
+      import('./features/countries/pages/country-detail/country-detail').then(
+        (m) => m.CountryDetail,
+      ),
+  },
 ];
